a59156dbd106fce524b0956c5366abed6d5c2e50
Francisco Prestes <franciscoprestes01@gmail.com>
2017-08-08 13:48:25 -0400

    Incremet Triangle

---
+++ GemotricShapesTDD/GemotricShapesTDD/Domain/Entities/TriangleTypes/EquilateralTriangle.cs
@@ -0,0 +1,9 @@
+﻿namespace GemotricShapesTDD.Domain.Entities.TriangleTypes
+{
+   public class EquilateralTriangle:Triangle
+    {
+        public EquilateralTriangle(double side1, double side2, double side3) : base(side1, side2, side3)
+        {
+        }
+    }
+}
---
+++ GemotricShapesTDD/GemotricShapesTDD/Domain/Entities/TriangleTypes/IsocelesTriangle.cs
@@ -0,0 +1,9 @@
+﻿namespace GemotricShapesTDD.Domain.Entities.TriangleTypes
+{
+   public class IsocelesTriangle:Triangle
+    {
+        public IsocelesTriangle(double side1, double side2, double side3) : base(side1, side2, side3)
+        {
+        }
+    }
+}
