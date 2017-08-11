fd2eed4da49b5f925cbea3cc569aa9d5fd9ca560
Francisco Prestes <franciscoprestes01@gmail.com>
2017-08-11 00:54:22 -0400

    Refactir triangle class

--- GemotricShapesTDD/GemotricShapesTDD/Domain/Entities/TriangleTypes/EquilateralTriangle.cs
+++ GemotricShapesTDD/GemotricShapesTDD/Domain/Entities/TriangleTypes/EquilateralTriangle.cs
@@ -1,6 +1,6 @@
 ﻿namespace GemotricShapesTDD.Domain.Entities.TriangleTypes
 {
-   public class EquilateralTriangle:Triangle
+    public class EquilateralTriangle:Triangle
     {
         public EquilateralTriangle(double side1, double side2, double side3) : base(side1, side2, side3)
         {
